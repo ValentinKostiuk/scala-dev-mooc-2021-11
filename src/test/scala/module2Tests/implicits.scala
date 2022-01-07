2c878378451ce4b41a27856f7575fe42e4de6aa0
ValentinKostiuk <valentin.kostiuk@gmail.com>
2022-01-07 21:05:42 +0200

    implementing tuple through bindable

---
+++ src/test/scala/module2Tests/implicits.scala
@@ -0,0 +1,15 @@
+package module2Tests
+
+import module2.homework_hkt_impllicts._
+import org.scalatest.flatspec.AnyFlatSpec
+
+class implicitsTests extends AnyFlatSpec {
+
+  "check two lists" should "merge into one" in {
+    assert(tupleF(List(1, 2, 3), List(4, 5, 6)) == List((1,4), (1,5), (1,6), (2,4), (2,5), (2,6), (3,4), (3,5), (3,6)))
+  }
+
+  "check two options" should "return option of tuple" in {
+     assert(tupleF(Option("here"), Option("there")) == Option(("here", "there")))
+   }
+}
