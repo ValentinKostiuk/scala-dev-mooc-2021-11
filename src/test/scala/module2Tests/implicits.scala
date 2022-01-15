921be4e5d7bc4f83dae5e5210eca904b260171f3
ValentinKostiuk <valentin.kostiuk@gmail.com>
2022-01-15 21:20:57 +0200

    reverting tests to master state

--- src/test/scala/module2Tests/implicits.scala
+++ src/test/scala/module2Tests/implicits.scala
@@ -1,16 +1,29 @@
 package module2Tests
 
-import module2.homework_typeClasses._
+import module2.homework_hkt_impllicts._
 import org.scalatest.flatspec.AnyFlatSpec
 
 class implicitsTests extends AnyFlatSpec {
 
-  // "check two lists" should "create list of all combinations tuple" in {
-  //   assert(tupleF(List(1, 2, 3), List(4, 5, 6)) == List((1,4), (1,5), (1,6), (2,4), (2,5), (2,6), (3,4), (3,5), (3,6)))
-  // }
+  "check two lists" should "create list of all combinations tuple" in {
+    assert(
+      tupleF(List(1, 2, 3), List(4, 5, 6)) == List((1, 4),(1, 5),(1, 6),(2, 4),(2, 5),(2, 6),(3, 4),(3, 5),(3, 6))
+    )
+  }
+
+  "check two lists" should "create list of all combinations tuple for different types" in {
+      assert(
+        tupleF(List(1, 2, 3), List("one", "two")) == List((1, "one"), (1, "two"), (2, "one"), (2, "two"), (3, "one"), (3,"two"))
+      )
+    }
 
   "check two options" should "return option of tuple" in {
-     assert(tupleF(Option("here"), Option("there")) == Option(("here", "there")))
-     assert(tupleF(Option(1), Option("something")) == Option((1, "something")))
-   }
+    assert(tupleF(Option("here"), Option("there")) == Option(("here", "there")))
+  }
+
+  "check two options" should "return option of tuple for different types" in {
+    assert(
+      tupleF(Option(1), Option("and a string")) == Option((1, "and a string"))
+    )
+  }
 }
